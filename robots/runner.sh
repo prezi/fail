ff24be119b169e12815e02ea47d60643ed34e4b3
Zoltan Nagy <abesto@abesto.net>
2014-06-27 10:52:43 +0200

    Run commands with bash -x, tells us when robots start and stop

--- robots/runner.sh
+++ robots/runner.sh
@@ -6,5 +6,5 @@ sleep=${1:-5}
 rm -f nohup.out && touch nohup.out
 tail -f nohup.out &
 tailer=$!
-nohup bash -c "./${robot}/start && sleep ${sleep} && ./${robot}/stop && sleep 1 && kill $tailer" 2>>nohup.out &
+nohup bash -cx "./${robot}/start && sleep ${sleep} && ./${robot}/stop && sleep 1 && kill $tailer" 2>>nohup.out &
 wait $! 2>/dev/null
