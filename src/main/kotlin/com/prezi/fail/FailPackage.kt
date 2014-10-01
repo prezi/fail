c6dd00a7d987217e707cdb57aab7aa6d23c00808
Zoltan Nagy <abesto@abesto.net>
2014-10-01 15:09:15 +0200

    Refactored cli parsing to use Commons CLI

--- src/main/kotlin/com/prezi/fail/FailPackage.kt
+++ src/main/kotlin/com/prezi/fail/FailPackage.kt
@@ -1,35 +1,47 @@
 package com.prezi.fail
 
-import com.prezi.fail.sarge.Sarge
-import com.prezi.changelog.ChangelogClient
+import java.util.Properties
 import java.io.File
 import java.io.FileInputStream
-import org.slf4j.LoggerFactory
-import com.prezi.fail.sarge.SargeConfig
-import java.util.Properties
+
 import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import ch.qos.logback.classic.Level
 
+import org.apache.commons.cli.Option
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.GnuParser
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.HelpFormatter
+
+import com.prezi.fail.sarge.Sarge
+import com.prezi.changelog.ChangelogClient
+import com.prezi.fail.sarge.SargeConfig
+
+
 private fun usage(exitCode: Int = 0) {
-    println("Usage: fail <tag> <sapper> <duration_seconds> [sapper_arg_1 [sapper_arg_2 ... ]]")
+    val formatter = HelpFormatter()
+    formatter.printHelp("fail tag sapper duration-seconds [sapper-arg [sapper-arg ...]]", Cli.options)
     System.exit(exitCode)
 }
 
-fun main(args: Array<String>) {
-    if (args.contains("--help")) {
-        usage()
+object Cli {
+    public val help: Option = Option("h", "help", false, "Display this help message")
+    public val debug: Option = Option("v", "debug", false, "Set root logger to DEBUG level")
+    public val trace: Option = Option("vv", "trace", false, "Set root logger to TRACE level")
+
+    public val options: Options = Options();
+
+    {
+        options.addOption(help)
+        options.addOption(debug)
+        options.addOption(trace)
     }
-    if (args.count() < 3) {
-        println("Not enough arguments.")
-        usage(1)
+
+    public fun parseCliArguments(args: Array<String>): CommandLine {
+        val parser = GnuParser()
+        return parser.parse(options, args)!!
     }
-    if (args.contains("--verbose") || args.contains("debug")) {
-        (LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME) as ch.qos.logback.classic.Logger).setLevel(Level.DEBUG)
-        return main(args.filterNot({ it == "--verbose"}).filterNot({ it == "--debug" }).copyToArray())
-    }
-    loadUserProperties()
-    // TODO: add logic for choosing action based on args[0] here
-    Sarge().charge(args[0], args[1], args[2], args.drop(3))
 }
 
 private fun loadUserProperties() {
@@ -51,5 +63,28 @@ private fun loadUserProperties() {
         logger.info("Loaded properties file ${file.canonicalPath}")
         appliedProperties.forEach { entry -> logger.debug("${file.canonicalPath}: ${entry.key} = ${entry.value}") }
     }
+}
+
+fun main(args: Array<String>) {
+    val commandLine = Cli.parseCliArguments(args)
+    if (commandLine.hasOption(Cli.help.getOpt())) {
+        usage()
+    }
+    if (commandLine.hasOption(Cli.debug.getOpt())) {
+        (LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME) as ch.qos.logback.classic.Logger).setLevel(Level.DEBUG)
+    }
+    if (commandLine.hasOption(Cli.trace.getOpt())) {
+        (LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME) as ch.qos.logback.classic.Logger).setLevel(Level.DEBUG)
+    }
+
+    val positionalArgs = commandLine.getArgs()!!
+    if (positionalArgs.count() < 3) {
+        println("Not enough arguments.")
+        usage(1)
+    }
+
+    loadUserProperties()
+    // TODO: add logic for choosing action based on args[0] here
+    Sarge().charge(positionalArgs[0], positionalArgs[1], positionalArgs[2], positionalArgs.drop(3))
 }
 
