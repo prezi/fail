17b9990e8d46c321c644e8912b7f7fcf71f89153
Zoltan Nagy <abesto@abesto.net>
2014-10-06 16:41:03 +0200

    A sudden healthcheck appears

---
+++ api/server/src/main/kotlin/com/prezi/fail/api/impl/HealthcheckResource.kt
@@ -0,0 +1,18 @@
+package com.prezi.fail.api.impl
+
+import com.linkedin.restli.server.annotations.RestLiCollection
+import com.linkedin.restli.server.resources.CollectionResourceTemplate
+import java.util.HashMap
+import com.prezi.fail.api.Healthcheck
+
+/**
+ * Very simple Rest.li Resource that serves up a fortune cookie.
+ */
+[RestLiCollection(name="healthcheck", namespace="com.prezi.fail")]
+public class FortunesResource: CollectionResourceTemplate<Long, Healthcheck>()
+{
+    override public fun get(key: Long?): Healthcheck?
+    {
+        return Healthcheck().setOk("Such OK!")
+    }
+}
