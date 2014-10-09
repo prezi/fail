662322c2be8a488bd43483137913dce7b544b305
Zoltan Nagy <abesto@abesto.net>
2014-10-09 14:25:54 +0200

    [api] Set up loggin

--- api/server/src/main/kotlin/com/prezi/fail/api/impl/ScheduledFailureResource.kt
+++ api/server/src/main/kotlin/com/prezi/fail/api/impl/ScheduledFailureResource.kt
@@ -4,10 +4,14 @@ import com.linkedin.restli.server.resources.CollectionResourceTemplate
 import com.prezi.fail.api.ScheduledFailure
 import com.linkedin.restli.server.annotations.RestLiCollection
 import com.linkedin.restli.server.CreateResponse
+import org.slf4j.LoggerFactory
 
 [RestLiCollection(name="ScheduledFailure", namespace="com.prezi.fail.api")]
 public class ScheduledFailureResource : CollectionResourceTemplate<Long, ScheduledFailure>() {
+    val logger = LoggerFactory.getLogger(javaClass)!!
+
     override public fun create(entity: ScheduledFailure?): CreateResponse {
+        logger.info("Scheduling ${entity}")
         return CreateResponse(42)
     }
 }
