b24e3362a91458bcec47043bab700939edd255dd
Zoltan Nagy <abesto@abesto.net>
2014-10-15 13:18:55 +0200

    Remove stupidness in api startup

--- api/server/src/main/kotlin/com/prezi/fail/api/ApiPackage.kt
+++ api/server/src/main/kotlin/com/prezi/fail/api/ApiPackage.kt
@@ -5,10 +5,10 @@ import com.prezi.fail.api.db.DBUsageExample
 import com.prezi.fail.loadUserProperties
 
 fun main(args: Array<String>) {
+    loadUserProperties("/etc/prezi/fail-api/fail-api.properties")
     if (args.size > 0 && args[0] == "dbtest") {
         DBUsageExample.run()
     } else {
-        loadUserProperties("/etc/prezi/fail-api/fail-api.properties")
         NettyStandaloneLauncher(8080, "com.prezi.fail.api.impl").start()
     }
 }
