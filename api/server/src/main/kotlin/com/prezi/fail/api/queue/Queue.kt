9b94e76f37d6b094c9140c34f4236330aba11e8d
Zsolt Dollenstein <zsol.zsol@gmail.com>
2014-10-17 18:29:29 +0200

    queue example

--- api/server/src/main/kotlin/com/prezi/fail/api/queue/Queue.kt
+++ api/server/src/main/kotlin/com/prezi/fail/api/queue/Queue.kt
@@ -3,14 +3,43 @@ package com.prezi.fail.api.queue
 import com.amazonaws.services.sqs.AmazonSQSClient
 import com.prezi.fail.api.ScheduledFailure
 import com.amazonaws.services.sqs.model.SendMessageRequest
+import com.prezi.fail.api.Run
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest
+import com.prezi.fail.api.db.DB
+import com.prezi.fail.api.db.DBRun
+import org.slf4j.LoggerFactory
+import com.amazonaws.AmazonServiceException
 
 object Queue {
     val client = AmazonSQSClient()
     val url = client.getQueueUrl("fail-scheduled-failures")?.getQueueUrl()
 
-    public fun putScheduledFailure(f: ScheduledFailure) {
+    internal val logger = LoggerFactory.getLogger(javaClass)
 
+    public fun putRun(r: Run) {
         val req = SendMessageRequest().withQueueUrl(url)
-        client.sendMessage(req.withMessageBody(""))
+        client.sendMessage(req.withMessageBody(r.getId()))
+    }
+
+    public fun receiveRunAnd(action: (Run) -> Unit): Unit {
+        val recvReq = ReceiveMessageRequest().withQueueUrl(url).withMaxNumberOfMessages(1)
+        val recvMsg = client.receiveMessage(recvReq)
+        val msg = recvMsg.getMessages().first
+        if (msg == null) {
+            logger.debug("Received empty message from SQS")
+            return
+        }
+        val key = DBRun().setId(msg.getBody())
+        val run = try {
+            DB.mapper.load(key)?.model
+        } catch (e: AmazonServiceException) {
+            logger.error("unable to load Run from DB for id ${msg.getBody()} because ${e}")
+            return
+        }
+        if (run == null) {
+            logger.error("invalid_run_id_in_sqs ${msg.getBody()}")
+            return
+        }
+        action(run)
     }
 }
--- api/server/src/main/kotlin/com/prezi/fail/api/queue/QueueExample.kt
+++ api/server/src/main/kotlin/com/prezi/fail/api/queue/QueueExample.kt
@@ -1,12 +1,28 @@
 package com.prezi.fail.api.queue
 
+import com.prezi.fail.api.db.DBRun
+import com.prezi.fail.api.RunStatus
+import com.prezi.fail.api.db.DBScheduledFailure
+import com.linkedin.data.template.StringArray
+import com.linkedin.data.template.StringMap
+import com.prezi.fail.api.db.DB
+
 fun main(args: Array<String>) {
-    val qUrl = Queue.url
-    val res = Queue.client.sendMessage(qUrl, "This is my item, woohoo!")
-    Queue.client.sendMessage(qUrl, "This is my item2, woohoo!")
-    Queue.client.sendMessage(qUrl, "This is my item3, woohoo!")
-    println("SendMessageResult: ${res.toString()}")
-    Queue.client.receiveMessage(com.amazonaws.services.sqs.model.ReceiveMessageRequest().withQueueUrl(qUrl).withMaxNumberOfMessages(1))
-    val recv = Queue.client.receiveMessage(qUrl)?.getMessages()
-    recv?.forEach { println("Item from queue: ${it.toString()}\n") }
+    val scheduledFailure = DBScheduledFailure()
+            .setPeriod("test-interval-42")!!
+            .setSapper("noop")!!
+            .setSearchTerm("hslogger-app")!!
+            .setConfiguration(StringMap())!!
+            .setDuration(30)!!
+            .setSapperArgs(StringArray())!!
+            .setScheduledAt(42)!!
+            .setScheduledBy("not-abesto")!!
+
+    val item = DBRun().setAt(System.currentTimeMillis() / 1000)?.setStatus(RunStatus.FAILED)?.setLog("testlog")?.setScheduledFailure(scheduledFailure)!!
+    DB.mapper.save(scheduledFailure)
+    DB.mapper.save(item)
+
+    Queue.putRun(item.model)
+    Queue.receiveRunAnd { println("Got run from queue ${it}") }
+
 }
