7a29fc3c7f59d4f4a9ae23cba89da5956cd91a2f
Zoltan Nagy <abesto@abesto.net>
2014-10-18 17:40:34 +0200

    Un-optimize some imports that actually need to be there

--- common/src/test/kotlin/com/prezi/fail/test/TestUtils.kt
+++ common/src/test/kotlin/com/prezi/fail/test/TestUtils.kt
@@ -1,6 +1,11 @@
 package com.prezi.fail.test
 
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when` as _when
+import org.mockito.stubbing.OngoingStubbing
+import org.mockito.internal.stubbing.defaultanswers.ReturnsDeepStubs
+import org.mockito.verification.VerificationMode
 
 public fun <T> givenAny(cls: Class<T>): T = mock(cls, ReturnsDeepStubs())!!
 public fun <T> When(methodCall: T): OngoingStubbing<T> = _when(methodCall)!!
