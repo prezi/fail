47573cb36298df810eb6c1080b416fa9fbc1a394
Zsolt Dollenstein <zsol.zsol@gmail.com>
2014-10-18 18:10:57 +0200

    wip test for runresource getter

--- api/server/src/test/kotlin/com/prezi/fail/api/impl/RunResourceTest.kt
+++ api/server/src/test/kotlin/com/prezi/fail/api/impl/RunResourceTest.kt
@@ -1,6 +1,28 @@
 package com.prezi.fail.api.impl
 
 import org.junit.Test
+import org.mockito.Matchers.*
+import com.prezi.fail.test.givenAny
+import com.prezi.fail.api.db.DB
+import com.prezi.fail.api.Run
+import com.prezi.fail.api.RunStatus
+import com.prezi.fail.api.ScheduledFailure
+import com.prezi.fail.test.When
+import kotlin.test.assertEquals
+import com.prezi.fail.api.db.DBRun
+import com.prezi.fail.api.db.DBScheduledFailure
+import org.junit.Ignore
 
 class RunResourceTest {
+    [Ignore]
+    Test fun getPopulatesScheduledFailure() {
+        val db = givenAny(javaClass<DB>())
+        val resource = RunResource(db)
+        val dbrun = DBRun().setId("1")?.setAt(0)?.setLog("testlog")?.setStatus(RunStatus.DONE)
+        val scheduled = DBScheduledFailure()
+        When(db.mapper.load<DBRun>(any())).thenReturn(dbrun)
+        When(db.mapper.load<DBScheduledFailure>(any())).thenReturn(scheduled)
+        val run = resource.get(dbrun?.getId())
+        assertEquals(scheduled.model, run?.getScheduledFailure())
+    }
 }
