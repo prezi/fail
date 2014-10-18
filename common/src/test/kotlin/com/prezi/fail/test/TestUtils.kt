a29eaec086fadbb266e3dda970dc2f13c0ee42e3
Zsolt Dollenstein <zsol.zsol@gmail.com>
2014-10-18 16:39:50 +0200

    givenAny() returns deep stubs

--- common/src/test/kotlin/com/prezi/fail/test/TestUtils.kt
+++ common/src/test/kotlin/com/prezi/fail/test/TestUtils.kt
@@ -5,8 +5,9 @@ import org.mockito.Mockito.`when` as _when
 import org.mockito.Mockito.verify
 import org.mockito.verification.VerificationMode
 import org.mockito.stubbing.OngoingStubbing
+import org.mockito.internal.stubbing.defaultanswers.ReturnsDeepStubs
 
-public fun <T> givenAny(cls: Class<T>): T = mock(cls)!!
+public fun <T> givenAny(cls: Class<T>): T = mock(cls, ReturnsDeepStubs())!!
 public fun <T> When(methodCall: T): OngoingStubbing<T> = _when(methodCall)!!
 
 public fun <T> Verify(methodCall: T): T = org.mockito.Mockito.verify(methodCall)!!
