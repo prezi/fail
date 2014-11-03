c35c8618d3a3484db76e88cc56acb6a261087df4
Zoltan Nagy <abesto@abesto.net>
2014-11-03 13:53:13 +0100

    Worker: use default AWS credentials provider chain

--- cli/src/main/kotlin/com/prezi/fail/sarge/Aws.kt
+++ cli/src/main/kotlin/com/prezi/fail/sarge/Aws.kt
@@ -5,8 +5,9 @@ import com.amazonaws.services.ec2.AmazonEC2
 import com.amazonaws.services.ec2.AmazonEC2Client
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.prezi.fail.config.SargeConfig
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 
 open class Aws(val config: SargeConfig) {
-    protected fun buildCredentials() : AWSCredentials? = ProfileCredentialsProvider().getCredentials()
+    protected fun buildCredentials() : AWSCredentials? = DefaultAWSCredentialsProviderChain().getCredentials()
     open fun ec2() : AmazonEC2 = AmazonEC2Client(buildCredentials())
 }
